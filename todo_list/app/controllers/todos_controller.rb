501086de6e225978fa0f7b8b9244b234a895a66b
uNickHow <NickLin@UserdeMacBook-Pro.local>
2017-03-09 12:19:05 +0800

    new & create actions

--- todo_list/app/controllers/todos_controller.rb
+++ todo_list/app/controllers/todos_controller.rb
@@ -1,2 +1,30 @@
 class TodosController < ApplicationController
+
+	def index
+		@todos = Todo.all
+		
+	end
+
+	def new
+		@todo = Todo.new
+
+	end
+
+	def create
+		@todo = Todo.new(todo_params)
+		@todo.save
+
+		redirect_to todos_path
+		
+	end
+
+
+
+	private
+
+	def todo_params
+		params.require(:todo).permit(:title, :due_date)
+		
+	end
+
 end
