351b80778ed9453f02b1e9cdd8caa59fd7bd77ea
uNickHow <NickLin@UserdeMacBook-Pro.local>
2017-03-10 14:49:44 +0800

    add flash msg

--- todo_list/app/controllers/todos_controller.rb
+++ todo_list/app/controllers/todos_controller.rb
@@ -3,7 +3,7 @@ class TodosController < ApplicationController
 	def index
 		@todos = Todo.all
 		@todos = Todo.order(due_date: :asc)
-		
+
 	end
 
 	def new
@@ -15,6 +15,7 @@ class TodosController < ApplicationController
 		@todo = Todo.new(todo_params)
 		if @todo.save
 			redirect_to todos_path   #重新發出request，會重整頁面
+			flash[:notice] = "List was successfully created !!"
 
 		else
 			render :action => :new   #保留需求不會清空資料，使用者可以繼續填寫錯誤的部分
@@ -31,6 +32,7 @@ class TodosController < ApplicationController
 	def update
 		@todo = Todo.find(params[:id])
 		@todo.update(todo_params)
+		flash[:notice] = "List was successfully updated !!"
 
 		redirect_to todos_path
 		
@@ -39,6 +41,7 @@ class TodosController < ApplicationController
 	def destroy
 		@todo = Todo.find(params[:id])
 		@todo.destroy
+		flash[:alert] = "List was successfully deleted !!"
 
 		redirect_to todos_path
 		
